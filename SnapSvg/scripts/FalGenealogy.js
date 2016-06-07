c7f29d2f638fe6d6265a09c3d864cf76d1cc233b
aymeric.fiorini <aymeric.fiorini@CND4347K0C>
2016-06-07 12:08:52 +0200

    Init

---
+++ SnapSvg/scripts/FalGenealogy.js
@@ -0,0 +1,37 @@
+﻿function FalGenealogyElement(params)
+{
+    GenealogyElement.Call(params)
+    if (params.velours == true) {
+        this.velours = true
+    }
+    else {
+        this.velours = false
+    }
+
+    this.filiere = {
+        name: params.filiereNom,
+        colors: params.filiereCouleurs
+    }
+    this.colors = this.element.gradient()
+}
+
+FalGenealogyElement.prototype = Object.create(GenealogyElement.prototype, {
+    applyColors: function () {
+        var nbColors = this.filiere.colors.length
+        if (nbColors == 1) {
+            this.element.rect.fill(this.filiere.colors[0])
+        }
+        else {
+            var colorPercentage = 1 / nbColors
+            var gradientString = ""
+            for (var i in this.filiere.colors) {
+                gradientString += this.filiere.colors[i] + " " + (colorPercentage * i).toString() + " " + this.filiere.colors[i] + " " + (colorPercentage * (i + 1)).toString() + " ";
+            }
+            //TODO: Syntaxe incorrecte
+            //TODO: créer méthode gradient, proxy de gradient dans GenealogyArea
+            var gradient = this.genealogyArea.gradient(gradientString)
+            this.element.rect.attr({ gradient: gradient })
+        }
+        
+    }
+})
---
+++ SnapSvg/scripts/app.js
@@ -0,0 +1,9 @@
+﻿document.getElementById('generateSvg').onclick = function() {
+    var s = Snap("#svg");
+
+    var circle = s.circle(200, 200, 150);
+    debugger;
+    var genealogyArea = new GenealogyArea("#svg")
+    var genEl = new GenealogyElement({}, genealogyArea)
+    genealogyArea.GenealogyElements.push(genEl)
+}
