171a0dc85a937f41a0937b706f4e9e204846c87a
aymeric.fiorini <aymeric.fiorini@CND4347K0C>
2016-06-20 15:09:04 +0200

    Typescript: ajout fichier

---
+++ SnapSvg/scripts/Genealogy.ts
@@ -0,0 +1,10 @@
+﻿class Genealogy {
+    svgArea: Snap;
+    mainArea: any;
+
+    constructor(svgZone: Snap) {
+        this.svgArea = Snap(svgZone)
+        this.mainArea = this.svgArea.group() // always use this if you want to move and scale in the area
+        
+    }
+}
--- SnapSvg/scripts/app.js
+++ SnapSvg/scripts/app.js
@@ -2,8 +2,13 @@
     var s = Snap("#svg");
 
     var circle = s.circle(200, 200, 150);
+    var f = s.filter(Snap.filter.shadow(-5, 0, 5, 'black', 1))
+    circle.attr({
+        filter: f
+    })
     debugger;
     var genealogyArea = new GenealogyArea("#svg")
     var genEl = new GenealogyElement({}, genealogyArea)
     genealogyArea.GenealogyElements.push(genEl)
+    
 }
